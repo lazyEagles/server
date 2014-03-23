9725f20534e46d7cb8f5d2e6f71f567c959372b2
YuShu Chen <lazyEagles@gmail.com>
2014-03-23 15:33:25 +0800

    cancel send CLOSE:

--- server_zerodavinci.c
+++ server_zerodavinci.c
@@ -14,12 +14,10 @@
 int main(int argc, char* argv[])
 {
 	struct sockaddr_in sin;
-	struct sockaddr_in cliaddr;
 	int buf[MAX_LINE];
-	int len = sizeof(cliaddr);
+	int len = 0;
 	int sd, new_sd;
 	FILE *fd;
-	struct stat st;
 	int file_size;
  
 	bzero((char*)&sin, sizeof(sin));
@@ -32,57 +30,54 @@ int main(int argc, char* argv[])
 		perror("simplex-talk: socket");
 		exit(EXIT_FAILURE);
 	}
- 
 	/* bind socket with address */
 	if ((bind(sd, (struct sockaddr*)&sin, sizeof(sin))) < 0) {
 		perror("simplex-talk: bind");
 		exit(EXIT_FAILURE);
 	}
- 
+	/* listen socket for connection */ 
 	listen(sd, MAX_PENDING);
  
 	while(1) {
+		/* accept a connection */
 		if ((new_sd = accept(sd, (struct sockaddr*) &sin, &len)) < 0) {
 			perror("simplex-talk: accept");
 			exit(EXIT_FAILURE);
 		}
 		int child = fork();
 		if (child == 0) {
-		const char *picname = "\"FILE:102062999.jpg\"";
-		const char get[20] = "\"GET:102062999.jpg\"";
-		const char *upload = "102062999.jpg";
-		strncpy((char *)buf, picname, MAX_LINE);
-		send(new_sd, buf, sizeof(buf), 0); // server send FILE:xx.jpg to client
-		recv(new_sd, buf, sizeof(buf), 0); // server recive GET:xxx.jpg
-		if (strncmp((char *)buf, get, strlen(get)) == 0) {
-			//stat(upload, &st);
-			//file_size = st.st_size; // how many bytes to send
-			fd = fopen(upload, "r");
-			int c;
-			int i;
-			do {
-				for (i = 0, c = 0; i < MAX_LINE && c != EOF; i++) {
-					c = fgetc(fd);
-					buf[i] = c;
-					//printf("%d:%x ", i, buf[i]);
-				}
-				int fuck = 0;
-				fuck = send(new_sd, buf, sizeof(buf), 0);
-				printf("we send %d\n",fuck);
-			} while(c != EOF);
-			fclose(fd);
-			const char *info = "\"CLOSE:\"";
-			strncpy((char *)buf, info, MAX_LINE);
-			send(new_sd, buf, sizeof(buf), 0);
-		}
-		printf("close socket\n");
-		close(new_sd);
-
-		}
-		else 
-		{
+			const char *filename = "\"FILE:102062650.txt\"";
+			const char get[20] = "\"GET:102062650.txt\"";
+			const char *upload = "102062650.txt";
+			strncpy((char *)buf, filename, MAX_LINE);
+			send(new_sd, buf, sizeof(buf), 0); // server send FILE:xx.jpg to client
+			recv(new_sd, buf, sizeof(buf), 0); // server recive GET:xxx.jpg
+			if (strncmp((char *)buf, get, strlen(get)) == 0) {
+				fd = fopen(upload, "r");
+				int c;
+				int i;
+				do {
+					for (i = 0, c = 0; i < MAX_LINE && c != EOF; i++) {
+						c = fgetc(fd);
+						buf[i] = c;
+						//printf("%d:%x ", i, buf[i]);
+					}
+					int sendsize = 0;
+					sendsize = send(new_sd, buf, sizeof(buf), 0);
+					printf("we send %d bytes\n",sendsize);
+				} while(c != EOF);
+				fclose(fd);
+				/* cancel fucntion: send "CLOSE:" for spec
+				*
+				* const char *info = "\"CLOSE:\"";
+				* strncpy((char *)buf, info, MAX_LINE);
+				* send(new_sd, buf, sizeof(buf), 0);
+				*/
+			}
+			printf("close socket\n");
 			close(new_sd);
-
+		} else {
+			close(new_sd);
 		}
 	}
 	return 0;
